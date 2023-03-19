fc24f6dd7309f13c04d5c927db529fa744d30acd
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-03-18 23:14:38 -0300

    knex configured

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,19 @@ import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', async () => {
+const start = async () => {
+  try {
+    await app.listen({ port: 3032 })
+    console.log('listening on port 3032')
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+app.get('/', async (req, res) => {
   const tables = await knex('sqlite_schema').select('*')
   return tables
 })
 
-app
-  .listen({
-    port: 3032,
-  })
-  .then(() => {
-    console.log('listening on port 3032')
-  })
+start()
